b5fa25a3c4f62086862f9a744d77c276f6ff4d50
Andrew James Boyle <109110419+andrewjamesboyle@users.noreply.github.com>
2022-09-13 16:15:42 -0700

    header and footer added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 
+import Header from './Components/Header/header';
 import Main from './Components/Main/main';
+import Footer from './Components/Footer/footer';
 
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
---
+++ src/Components/Footer/footer.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Footer() {
+  return <footer>Copyright Alchemy Astrology Lab 2022</footer>;
+}
